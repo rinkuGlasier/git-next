beb81f8745656994ab4c2c671f3ee3a6d8a05424
rinkuGlasier <rinku@glasier.in>
2024-09-27 10:45:53 +0530

    first commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from './font';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white bg-blue-500 py-10 rounded-xl px-5 `}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <p className={`${lusitana.className} text-[44px] `}>Acme</p>
     </div>
   );
 }
