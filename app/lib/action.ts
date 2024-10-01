fb1e83b8d9d13928cb6dde160e19e6c04800b96e
rinkuGlasier <rinku@glasier.in>
2024-10-01 16:23:33 +0530

    pagination , edit, delete

---
+++ app/lib/action.ts
@@ -0,0 +1,35 @@
+'use server'
+// Use Zod to update the expected types
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.string(),
+    date: z.string(),
+});
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+    revalidatePath('/dashboard/invoices');
+}
